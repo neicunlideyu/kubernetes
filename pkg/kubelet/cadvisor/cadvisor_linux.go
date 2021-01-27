d622ca41704c736686521d8db03f3fcd07a45274
luodaowen.backend <luodaowen.backend@bytedance.com>
2021-01-27 16:16:05 +0800

    fix(kubelet): update cadvisor api for compatibility

--- pkg/kubelet/cadvisor/cadvisor_linux.go
+++ pkg/kubelet/cadvisor/cadvisor_linux.go
@@ -46,6 +46,7 @@ import (
 	"github.com/google/cadvisor/manager"
 	"github.com/google/cadvisor/utils/sysfs"
 	"k8s.io/klog"
+	"k8s.io/utils/pointer"
 )
 
 type cadvisorClient struct {
@@ -99,9 +100,15 @@ func New(imageFsInfoProvider ImageFsInfoProvider, rootPath string, cgroupRoots [
 	if usingLegacyStats {
 		includedMetrics[cadvisormetrics.DiskUsageMetrics] = struct{}{}
 	}
+	duration := maxHousekeepingInterval
+	housekeepingConfig := manager.HouskeepingConfig{
+		Interval:     &duration,
+		AllowDynamic: pointer.BoolPtr(allowDynamicHousekeeping),
+	}
 
 	// Create the cAdvisor container manager.
-	m, err := manager.New(memory.New(statsCacheDuration, nil), sysFs, maxHousekeepingInterval, allowDynamicHousekeeping, includedMetrics, http.DefaultClient, cgroupRoots)
+	m, err := manager.New(memory.New(statsCacheDuration, nil), sysFs, housekeepingConfig, includedMetrics, http.DefaultClient, cgroupRoots, "")
+	// Create the cAdvisor container manager.
 	if err != nil {
 		return nil, err
 	}
